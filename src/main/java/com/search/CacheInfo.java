ba5418454eac74ab1b80902fa438dc099b2e98de
yashskullfox <yashpatel@Yashs-MacBook-Pro.local>
2021-03-05 23:25:20 -0800

    added lombok

--- src/main/java/com/search/CacheInfo.java
+++ src/main/java/com/search/CacheInfo.java
@@ -1,8 +1,17 @@
 package com.search;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.time.Instant;
 
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
 public class CacheInfo implements Serializable {
     /* This class is Cache Data stored in Cache Bucket */
 
@@ -10,43 +19,4 @@ public class CacheInfo implements Serializable {
     private String type;
     private Long value;
     Instant lastModification;
-
-    public CacheInfo(int accoutnNumber, String type, Long value, Instant lastModification) {
-        this.accoutnNumber = accoutnNumber;
-        this.type = type;
-        this.value = value;
-        this.lastModification = lastModification;
-    }
-
-    public int getAccoutnNumber() {
-        return accoutnNumber;
-    }
-
-    public void setAccoutnNumber(int accoutnNumber) {
-        this.accoutnNumber = accoutnNumber;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Long getValue() {
-        return value;
-    }
-
-    public void setValue(Long value) {
-        this.value = value;
-    }
-
-    public Instant getLastModification() {
-        return lastModification;
-    }
-
-    public void setLastModification(Instant lastModification) {
-        this.lastModification = lastModification;
-    }
 }
